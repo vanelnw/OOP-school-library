484d641398757b7e6cdfa5b05502cb157eedbd01
Vanel <vanelnwaba@gmail.com>
2023-02-02 01:50:23 +0100

    Add Nameable  class and decorators

--- person.rb
+++ person.rb
@@ -1,9 +1,16 @@
 # Class that represents a person
-class Person
+class Nameable
+  def correct_name
+    raise NotImplementedError, 'correct_name method must be implemented in a subclass.'
+  end
+end
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super
     @id = rand(100..1000)
     @name = name
     @age = age
@@ -21,4 +28,40 @@ class Person
   def can_use_services?
     is_of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
 end
+
+class Decorator < Nameable
+  def initialize(nameable)
+    super
+    @wrapper = nameable
+  end
+
+  def correct_name
+    @wrapper.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @wrapper.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @wrapper.correct_name[0...10]
+  end
+end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
