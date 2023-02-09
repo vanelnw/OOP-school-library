8b888ec257edd457dfc816f106b1a8e67a0ab0c9
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-09 19:59:00 +0200

    Add the method to the boo file

--- book.rb
+++ book.rb
@@ -12,5 +12,15 @@ class Book
     @rentals << rental
   end
 
-  
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'title' => @title,
+      'author' => @author
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    new(object['title'], object['author'])
+  end
 end
